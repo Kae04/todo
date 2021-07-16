1c0e22d471273c2dcb5762b066132a04489086c3
Kae04 <kaede.kajiro@gmail.com>
2021-07-17 03:51:17 +0900

    first-comit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\support\Facades\DB;
+
+class TodoController extends Controller
+{
+    public function index(Request $request)
+    {
+        $items = DB::select('select * from people');
+        return view('index', ['items' => $items]);
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.hello')
+<style>
+   th {
+      
+      color: black;
+      padding: 5px 30px;
+    }
+    td {
+      border: 1px solid black;
+      padding: 5px 30px;
+      text-align: center;
+    }
+</style>
+@section('title', 'index.blade.php')
+
+@section('content')
+
+<table>
+  <tr>
+    <th>作成日</th>
+    <th>タスク名</th>
+    <th>更新</th>
+    <th>削除</th>
+  </tr>
+  @foreach ($items as $item)
+  <tr>
+    <td>
+      {{$item->作成日}}
+    </td>
+    <td>
+      {{$item->タスク名}}
+    </td>
+    <td>
+      {{$item->更新}}
+    </td>
+    <td>
+      {{$item->削除}}
+    </td>
+  </tr>
+  @endforeach
+</table>
+@endsection
